bf8ac498ef95d7e9fd6eb8e25c97a5093b8c3631
Funny_Silkie <ban.grumble99rar@gmail.com>
2020-05-10 01:04:30 +0900

    fix chap.02-indent

--- Tutorials/Chap2/Spl1.cs
+++ Tutorials/Chap2/Spl1.cs
@@ -11,17 +11,17 @@ namespace Tutorial
             // エンジンを初期化
             Engine.Initialize("Tutorial", 960, 720);
 
-            // 自機
-+            var player = new SpriteNode();
-+            // 自機のテクスチャを読み込む
-+            player.Texture = Texture2D.LoadStrict("Resources/Player.png");
-+            // 自機の座標を設定
-+            player.Position = new Vector2F(100, 360);
-+            // 自機の中心座標を設定
-+            player.CenterPosition = player.Texture.Size / 2;
++           // 自機
++           var player = new SpriteNode();
++           // 自機のテクスチャを読み込む
++           player.Texture = Texture2D.LoadStrict("Resources/Player.png");
++           // 自機の座標を設定
++           player.Position = new Vector2F(100, 360);
++           // 自機の中心座標を設定
++           player.CenterPosition = player.Texture.Size / 2;
 +
-+            // 自機をエンジンに追加
-+            Engine.AddNode(player);
++           // 自機をエンジンに追加
++           Engine.AddNode(player);
 
             // メインループ
             while (Engine.DoEvents())
